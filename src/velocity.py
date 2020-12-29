ae4fd13935360a5ce3050104206d8f1a98d66a28
Marcin Wierzbiński <marcin119a@gmail.com>
2020-12-29 23:32:45 +0100

    answer 1 question

--- src/velocity.py
+++ src/velocity.py
@@ -8,13 +8,21 @@ def velocity_on_geoid(lat1, lon1, lat2, lon2, date1, date2):
 
     elapsed_time = 0
 
-    start_time = datetime.strptime(date1, '%Y-%m-%d %H:%M:%S')
-    end_time = datetime.strptime(date2, '%Y-%m-%d %H:%M:%S')
+    try:
+        start_time = datetime.strptime(date1, '%Y-%m-%d %H:%M:%S')
+        end_time = datetime.strptime(date2, '%Y-%m-%d %H:%M:%S')
+        diff = end_time - start_time
+        elapsed_time = diff.seconds
+    except:
+        return 0
 
-    diff = end_time - start_time
-    elapsed_time = diff.seconds
     dist = distance_on_geoid(lat1, lon1, lat2, lon2)
-    speed_mps = dist / elapsed_time
+    try:
+        speed_mps = dist / elapsed_time
+    except ZeroDivisionError:
+        speed_mps = 0
+
+    
     speed_kph = (speed_mps * 3600.0) / 1000.0
     
     return speed_kph
--- src/velocity_analytics.py
+++ src/velocity_analytics.py
@@ -1,10 +1,13 @@
 import pandas as pd
-
+from velocity import velocity_on_geoid
 
 int_1 = pd.read_json('/home/mw/Matematyka/projekt-python/data/test_0.json')
 int_1 = pd.json_normalize(int_1['result'])
-print(int_1)
 int_2 = pd.read_json('/home/mw/Matematyka/projekt-python/data/test_1.json')
 int_2 = pd.json_normalize(int_2['result'])
 
-y = pd.merge(int_1, int_2, left_on='VehicleNumber', right_on='VehicleNumber', how='left').drop('VehicleNumber', axis=1)
+df = pd.merge(int_1, int_2, left_on='VehicleNumber', right_on='VehicleNumber', how='left').drop('VehicleNumber', axis=1)
+for index, row in df.iterrows():
+    kph = velocity_on_geoid(row['Lat_x'], row['Lon_x'], row['Lat_y'], row['Lon_y'], row['Time_x'], row['Time_y'])
+    if  kph > 50:
+        print(kph)
